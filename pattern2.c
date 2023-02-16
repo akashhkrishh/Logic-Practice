b65b44fd0e58b2fdacaddc3fb5208eeb48ae8015
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-02-16 11:36:09 +0530

    Create pattern2.c

---
+++ pattern2.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n=5,c=0,p=1;
+   // scanf("%d",&n);
+   for(int k=1;k<=n;k++){
+        printf("%d ",p);    
+        }
+        printf("\n");
+    for(int k=2;k<n;k++){
+        printf("%d ",p);
+        for(int k=1;k<=n-2;k++){
+        printf("%d ",c++);
+    }
+    printf("%d ",p);
+    printf("\n");
+    }
+    for(int k=1;k<=n;k++){
+            printf("%d ",p);
+        }
+ 
+}
