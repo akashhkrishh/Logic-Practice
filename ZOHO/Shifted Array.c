15b40bc1041cf178557f86360eb2e4241ae4df6c
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-05-23 16:07:08 +0530

    Update Shifted Array.c

--- ZOHO/Shifted Array.c
+++ ZOHO/Shifted Array.c
@@ -3,11 +3,11 @@
 int main() {
     int N,C;
     scanf("%d %d",&N,&C);
-    for(int i=(N-C)+2;i<=N;i++){
-        printf("%d ",i);
-    }
-    for(int i=1;i<=(N-C)+1;i++){
-        printf("%d ",i);
+    int snum=1;
+    for(int i=1;i<=N;i++){
+        int diff=((i-C)+N)%N;
+        int num=snum+diff;
+        printf("%d ",num);
     }
     return 0;
 }
