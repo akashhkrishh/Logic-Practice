c194bacd719b78b459c0cd2b0401884ff0d4b7fe
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-06-11 21:44:56 +0530

    Update SpoonSp*n.c

--- Code Practice /SpoonSp*n.c
+++ Code Practice /SpoonSp*n.c
@@ -1,4 +1,4 @@
-/******************************************************************************
+/* *****************************************************************************
 
 Print true if second string is a substring of first string, else print false. [ZOHO]
 Note: * symbol can replace n number of characters
@@ -8,7 +8,7 @@ Zoho *o*o Output : TRUE
 Man n* Output : FALSE
 Subline *line Output : TRUE
 
-*******************************************************************************/
+****************************************************************************** */
 #include <stdio.h>
 #include <string.h>
 
@@ -17,30 +17,31 @@ int main()
     char s[100];
     char ns[100];
     scanf("%s %s",s,ns);
-    int c=0;
-    int k=0,co=0;
-    for(int i=0;i<strlen(s);i++){
-        if(c==i){
-            co++;
+    int c=0,k=0,co=0;
+    if(strlen(s)==strlen(ns)){
+        for(int i=0;i<strlen(s);i++){
+            if(s[i]==ns[i] && ns[i]!='*'){
+                co++;
+            }
+            if(s[i]!=ns[i] && ns[i]=='*'){
+                co++;
+            }
         }
-        if((ns[k]=='*' && s[i]!=ns[k] ) || s[i]==ns[k]){
-            c++;
-        }
-        if(s[i]==ns[k]){
-            k++;
-          
-        }
-        
-        if(ns[k]=='*' && s[i+1]==ns[k+1] ){
-            k++;
-         
-        }
-        
-        
-        
     }
-    (strlen(s)==co)?printf("TRUE"):printf("FALSE");
-    
-
+    else if(strlen(s)<strlen(ns)){
+        co=0;
+    }
+    else{
+        for(int i=0;i<strlen(s);i++){
+            (c==i)?co++:0;      
+            if((ns[k]=='*' && s[i]!=ns[k] ) || s[i]==ns[k]){
+                c++;
+            }
+            if(s[i]==ns[k] || (ns[k]=='*' && s[i+1]==ns[k+1])  ){
+                k++;
+            } 
+        }
+    }
+    (strlen(s)==co )?printf("TRUE"):printf("FALSE");
     return 0;
 }
