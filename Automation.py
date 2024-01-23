67ab949cb925a497d6d621e50aa9ced27f7f394a
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2024-01-23 21:04:12 +0530

    Update Automation.py

--- Automation.py
+++ Automation.py
@@ -1,22 +1,22 @@
 import time
 from datetime import datetime
 import pyautogui as pyauto
-times = ["20:09","20:10","20:11","20:12","20:13","20:30",]
-time.sleep(1)
+times = ["09:09","10:01","10:10","11:11","12:12","12:21","12:34","01:01","01:11","01:23","02:02","02:22"]
+time.sleep(5)
 while True: 
-    time.sleep(1)
-    current_time = datetime.now().strftime("%H:%M")
-    if(len(times) == 0): break
-    if(times[0]<current_time ):
-        times.pop(0)
-    if(len(times) != 0 and times[0]==current_time):
+    time.sleep(0.7)
+    current_time = datetime.now().time().strftime("%I:%M")
+    if(len(times) == 0 ): 
+        break
+    if(times[0]==current_time):
         msg = times.pop(0)
         data = msg.split(":")
-        convert = lambda x: x - 12 if x > 12 else x
-        d = convert(int(data[0]))
-        print(msg)
-        pyauto.write((str(d))+""+data[1])
+        pyauto.write(str(int(data[0]))+data[1])
         pyauto.press("enter")
+    elif(times[0] < current_time):
+        times.pop(0)
+    else:
+        print(times[0])
     
 
     
