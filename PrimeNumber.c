98eff946273fb3348ebbe549ed60dfea50625f41
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2024-06-30 09:16:35 +0530

    Create PrimeNumber.c

---
+++ PrimeNumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+void isPrime(int prime){
+    int flag = 0;
+    for(int i=2;i<=sqrt(prime) && flag==0;flag=(prime%(i++)==0)?1:0);
+    if(!flag)
+        printf("%d ",prime);
+}
+
+int main()
+{
+    
+    for(int i=2;i<=100;i++){
+        isPrime(i);
+    }
+    return 0;
+}
